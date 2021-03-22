a6456ee0a3d9a42b885f34d21220cd5884e1bd79
Buer <80929626+Bael0@users.noreply.github.com>
2021-03-22 00:44:21 -0600

    Add files via upload

--- noel.py
+++ noel.py
@@ -4,29 +4,33 @@ import sys
 import os
 import requests
 
-
-
 def fcBin():
     os.system("clear")
     print(pyfiglet.figlet_format("NOEL", font = "slant"))
     print("++++++++++++++++++++++++++++++")
     resp = input  ("1.friend of firend\n"+ "2.information gathering\n" + "3.facebook info\n")
     print("++++++++++++++++++++++++++++++")
-
     if(resp =="1"):
-        print ("no script yet <(")
+        print(pyfiglet.figlet_format("NOEL", font = "slant"))
+        print("+++++++++++++++++++++++++++++")
+        print("login to get your friends)
+        resp1 input = ("username:")
+        resp2 input = ("password:")
+        rr = requests.get("https://graph.facebook.com/oauth/access_token?grant_type=client_credentials&resp1=123&client_secret=resp2 ")
         sys.exit()
+        print("++++++++++++++++++++++++++++++")
     elif(resp == "2"):
         print ("no script yet <(")
         sys.exit()
-
-
-
+    elif(resp ==  "3"):
+        fcBin()
+    else:
+        print("this is not a option")
 def info():
     os.system("clear")
     print(pyfiglet.figlet_format("NOEL", font = "slant"))
     print ("+++++++++++++++++++++++")
-    resp = input  ("1.phishing\n"+ "2.information gathering\n" + "3.facebook info\n")
+    resp = input  ("1.account hunter\n"+ "2.information gathering\n" + "3.facebook info\n")
     print ("++++++++++++++++++++++++++++")
 
     if(resp =="1"):
@@ -35,6 +39,8 @@ def info():
     elif(resp == "2"):
         print ("no script yet <(")
         sys.exit()
-    else:
+    elif(resp ==  "3"):
         fcBin()
+    else:
+        print("this is not a option")
 info()
